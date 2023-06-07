bb91d933adc0f4f350c87b69eece33bdbc5163ca
Emmanuel <emmanuelrub_@hotmail.com>
2023-06-07 12:04:52 -0600

    Inicio del proyecto y del repositorio, creando componentes y carpetas

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar(){
+  return(
+    <div>
+      <nav>
+        Sotre Logo
+        <ul>
+          <li>
+            Cart items: 
+            <span className="cart-count">
+              0
+            </span> 
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+export default Navbar;
